6894d3cea443ebadd9d912f1bcfe7e217ba7fc84
RuthAngus <ruth.angus@astro.ox.ac.uk>
2018-03-13 17:41:27 -0400

    a little more documentation

--- code/plotting.py
+++ code/plotting.py
@@ -54,7 +54,11 @@ def plot_obs_and_get_bin_edges(kois, var_names, nbins, ranges, log=False):
     return bins
 
 
-def plot_marginal_completeness_2d(x_grid, y_grid, marg_comp, xlabel, ylabel, contours=False):
+def plot_marginal_completeness_2d(x_grid, y_grid, marg_comp, xlabel, ylabel,
+                                  contours=False):
+    """
+    Make a 2d histogram plot of the completeness as a function of x and y.
+    """
     pl.pcolor(x_grid, y_grid, marg_comp, cmap="BuGn")
 
     if contours:
@@ -70,6 +74,9 @@ def plot_marginal_completeness_2d(x_grid, y_grid, marg_comp, xlabel, ylabel, con
 
 
 def plot_det_eff_2d(grids, comp, label):
+    """
+    Plot the 2d histogram of the detection efficiency over 3 dimensions.
+    """
     period_grid, rp_grid, c_grid = grids
 
     # period = 9, radius = 11, c = 10
@@ -78,12 +85,18 @@ def plot_det_eff_2d(grids, comp, label):
     comp_mop = np.sum(comp, axis=0)  # Marginalize over period.
 
     # Plot the three combinations
-    plot_marginal_completeness_2d(period_grid[:, :, 0], rp_grid[:, :, 0], comp_moc, "Period", "$R_p \, [R_\oplus]$")
-    plot_marginal_completeness_2d(period_grid[:, 0, :], c_grid[:, 0, :], comp_mor, "Period", label)
-    plot_marginal_completeness_2d(rp_grid[0, :, :], c_grid[0, :, :], comp_mop, "$R_p \, [R_\oplus]$", label)
+    plot_marginal_completeness_2d(period_grid[:, :, 0], rp_grid[:, :, 0],
+                                  comp_moc, "Period", "$R_p \, [R_\oplus]$")
+    plot_marginal_completeness_2d(period_grid[:, 0, :], c_grid[:, 0, :],
+                                  comp_mor, "Period", label)
+    plot_marginal_completeness_2d(rp_grid[0, :, :], c_grid[0, :, :],
+                                  comp_mop, "$R_p \, [R_\oplus]$", label)
 
 
-def plot_marginal_completeness_1d(x_grid, marg_comp, xlabel, ylabel, contours=False):
+def plot_marginal_completeness_1d(x_grid, marg_comp, xlabel, ylabel)
+    """
+    Make a 1d histogram of the completeness as a function of x.
+    """
     pl.step(x_grid, marg_comp)
     pl.xlabel("{}".format(xlabel))
     pl.ylabel(ylabel);
@@ -91,6 +104,9 @@ def plot_marginal_completeness_1d(x_grid, marg_comp, xlabel, ylabel, contours=Fa
 
 
 def plot_det_eff_1d(grids, comp, var_names):
+    """
+    Plot the 1d histogram of the detection efficiency over 2 dimensions.
+    """
     period_grid, rp_grid, c_grid = grids
 
     comp_c = np.sum(comp, axis=(0, 1))  # Marginalize over period and radius
@@ -98,12 +114,19 @@ def plot_det_eff_1d(grids, comp, var_names):
     comp_r = np.sum(comp, axis=(0, 2))  # Marginalize over period and stellar param
 
     # Plot the three combinations
-    plot_marginal_completeness_1d(period_grid[:, 0, 0], comp_p, var_names[0], "Completeness")
-    plot_marginal_completeness_1d(rp_grid[0, :, 0], comp_r, var_names[1], "Completeness")
-    plot_marginal_completeness_1d(c_grid[0, 0, :], comp_c, var_names[2], "Completeness")
+    plot_marginal_completeness_1d(period_grid[:, 0, 0], comp_p, var_names[0],
+                                  "Completeness")
+    plot_marginal_completeness_1d(rp_grid[0, :, 0], comp_r, var_names[1],
+                                  "Completeness")
+    plot_marginal_completeness_1d(c_grid[0, 0, :], comp_c, var_names[2],
+                                  "Completeness")
 
 
 def plot_occ_rates(kois, grids, comp, var_names, bins):
+    """
+    Plot the observed distribution divided by the detection efficiency over 3
+    dimensions.
+    """
     period_grid, rp_grid, c_grid = grids
 
     comp_c = np.sum(comp, axis=(0, 1))  # Marginalize over period and radius
@@ -111,10 +134,13 @@ def plot_occ_rates(kois, grids, comp, var_names, bins):
     comp_r = np.sum(comp, axis=(0, 2))  # Marginalize over period and stellar param
 
     hist_p, _ = np.histogram(kois["{}".format(var_names[0])], bins[0])
-    plot_marginal_completeness_1d(period_grid[:-1, 0, 0], hist_p/comp_p[:-1], var_names[0], "Occurrence rate")
+    plot_marginal_completeness_1d(period_grid[:-1, 0, 0], hist_p/comp_p[:-1],
+                                  var_names[0], "Occurrence rate")
 
     hist_r, _ = np.histogram(kois["{}".format(var_names[1])], bins[1])
-    plot_marginal_completeness_1d(rp_grid[0, :-1, 0], hist_r/comp_r[:-1], var_names[1], "Occurrence rate")
+    plot_marginal_completeness_1d(rp_grid[0, :-1, 0], hist_r/comp_r[:-1],
+                                  var_names[1], "Occurrence rate")
 
     hist_c, _ = np.histogram(kois["{}".format(var_names[2])], bins[2])
-    plot_marginal_completeness_1d(c_grid[0, 0, :-1], hist_c/comp_c[:-1], var_names[2], "Occurrence rate")
+    plot_marginal_completeness_1d(c_grid[0, 0, :-1], hist_c/comp_c[:-1],
+                                  var_names[2], "Occurrence rate")
